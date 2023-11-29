3b4a1d91429d5f6ddf4b7eb62994c51ead765151
Baptiste <baptiste@humanoid.fr>
2023-11-29 16:26:03 +0100

    forked

---
+++ README.md
@@ -0,0 +1,5 @@
+# 
+
+Fully based on companion-module-generic-osc, I own no copyright on neither companion nor gabin, nor the companion-module-generic-osc code
+
+Gabin can be piloted from the osc generic plugin, but this module add feedbacks and convenient functions
---
+++ osc.js
@@ -0,0 +1,276 @@
+const { InstanceBase, Regex, runEntrypoint } = require('@companion-module/base')
+const UpgradeScripts = require('./upgrades')
+
+class OSCInstance extends InstanceBase {
+	constructor(internal) {
+		super(internal)
+	}
+
+	async init(config) {
+		this.config = config
+
+		this.updateStatus('ok')
+
+		this.updateActions() // export actions
+	}
+	// When module gets deleted
+	async destroy() {
+		this.log('debug', 'destroy')
+	}
+
+	async configUpdated(config) {
+		this.config = config
+	}
+
+	// Return config fields for web config
+	getConfigFields() {
+		return [
+			{
+				type: 'textinput',
+				id: 'host',
+				label: 'Target IP',
+				width: 8,
+				regex: Regex.IP,
+			},
+			{
+				type: 'textinput',
+				id: 'port',
+				label: 'Target Port',
+				width: 4,
+				regex: Regex.PORT,
+			},
+            {
+				type: 'textinput',
+				id: 'serverport',
+                label: 'Reply Port',
+				width: 4,
+				regex: Regex.PORT,
+			}
+		]
+	}
+
+	updateActions() {
+		const sendOscMessage = (path, args) => {
+			this.log('debug', `Sending OSC ${this.config.host}:${this.config.port} ${path}`)
+			this.log('debug', `Sending Args ${JSON.stringify(args)}`)
+			this.oscSend(this.config.host, this.config.port, path, args)
+		}
+
+		this.setActionDefinitions({
+			send_blank: {
+				name: 'Send message without arguments',
+				options: [
+					{
+						type: 'textinput',
+						label: 'OSC Path',
+						id: 'path',
+						default: '/osc/path',
+						useVariables: true,
+					},
+				],
+				callback: async (event) => {
+					const path = await this.parseVariablesInString(event.options.path)
+
+					sendOscMessage(path, [])
+				},
+			},
+			send_int: {
+				name: 'Send integer',
+				options: [
+					{
+						type: 'textinput',
+						label: 'OSC Path',
+						id: 'path',
+						default: '/osc/path',
+						useVariables: true,
+					},
+					{
+						type: 'textinput',
+						label: 'Value',
+						id: 'int',
+						default: 1,
+						regex: Regex.SIGNED_NUMBER,
+						useVariables: true,
+					},
+				],
+				callback: async (event) => {
+					const path = await this.parseVariablesInString(event.options.path)
+					const int = await this.parseVariablesInString(event.options.int)
+
+					sendOscMessage(path, [
+						{
+							type: 'i',
+							value: parseInt(int),
+						},
+					])
+				},
+			},
+			send_float: {
+				name: 'Send float',
+				options: [
+					{
+						type: 'textinput',
+						label: 'OSC Path',
+						id: 'path',
+						default: '/osc/path',
+						useVariables: true,
+					},
+					{
+						type: 'textinput',
+						label: 'Value',
+						id: 'float',
+						default: 1,
+						regex: Regex.SIGNED_FLOAT,
+						useVariables: true,
+					},
+				],
+				callback: async (event) => {
+					const path = await this.parseVariablesInString(event.options.path)
+					const float = await this.parseVariablesInString(event.options.float)
+
+					sendOscMessage(path, [
+						{
+							type: 'f',
+							value: parseFloat(float),
+						},
+					])
+				},
+			},
+			send_string: {
+				name: 'Send string',
+				options: [
+					{
+						type: 'textinput',
+						label: 'OSC Path',
+						id: 'path',
+						default: '/osc/path',
+						useVariables: true,
+					},
+					{
+						type: 'textinput',
+						label: 'Value',
+						id: 'string',
+						default: 'text',
+						useVariables: true,
+					},
+				],
+				callback: async (event) => {
+					const path = await this.parseVariablesInString(event.options.path)
+					const string = await this.parseVariablesInString(event.options.string)
+
+					sendOscMessage(path, [
+						{
+							type: 's',
+							value: '' + string,
+						},
+					])
+				},
+			},
+			send_multiple: {
+				name: 'Send message with multiple arguments',
+				options: [
+					{
+						type: 'textinput',
+						label: 'OSC Path',
+						id: 'path',
+						default: '/osc/path',
+						useVariables: true,
+					},
+					{
+						type: 'textinput',
+						label: 'Arguments',
+						id: 'arguments',
+						default: '1 "test" 2.5',
+						useVariables: true,
+					},
+				],
+				callback: async (event) => {
+					const path = await this.parseVariablesInString(event.options.path)
+					const argsStr = await this.parseVariablesInString(event.options.arguments)
+
+					const rawArgs = (argsStr + '').replace(/“/g, '"').replace(/”/g, '"').split(' ')
+
+					if (rawArgs.length) {
+						const args = []
+						for (let i = 0; i < rawArgs.length; i++) {
+							if (rawArgs[i].length == 0) continue
+							if (isNaN(rawArgs[i])) {
+								let str = rawArgs[i]
+								if (str.startsWith('"')) {
+									//a quoted string..
+									while (!rawArgs[i].endsWith('"')) {
+										i++
+										str += ' ' + rawArgs[i]
+									}
+								} else if(str.startsWith('{')) {
+									//Probably a JSON object
+									try {
+										args.push((JSON.parse(rawArgs[i])))
+									} catch (error) {
+										this.log('error', `not a JSON object ${rawArgs[i]}`)
+									}
+								}
+
+								args.push({
+									type: 's',
+									value: str.replace(/"/g, '').replace(/'/g, ''),
+								})
+							} else if (rawArgs[i].indexOf('.') > -1) {
+								args.push({
+									type: 'f',
+									value: parseFloat(rawArgs[i]),
+								})
+							} else {
+								args.push({
+									type: 'i',
+									value: parseInt(rawArgs[i]),
+								})
+							}
+						}
+
+						sendOscMessage(path, args)
+					}
+				},
+			},
+			send_boolean: {
+				name: 'Send boolean',
+				options: [
+					{
+						type: 'static-text',
+						label: 'Attention',
+						value: 'The boolean type is non-standard and may only work with some receivers.',
+						id: 'warning'
+					},
+					{
+						type: 'textinput',
+						label: 'OSC Path',
+						id: 'path',
+						default: '/osc/path',
+						useVariables: true,
+					},
+					{
+						type: 'checkbox',
+						label: 'Value',
+						id: 'value',
+						default: false,
+					},
+				],
+				callback: async (event) => {
+					const path = await this.parseVariablesInString(event.options.path)
+					let type = 'F'
+					if (event.options.value === true) {
+						type = 'T'
+					}
+
+					sendOscMessage(path, [
+						{
+							type,
+						},
+					])
+				},
+			},
+		})
+	}
+}
+
+runEntrypoint(OSCInstance, UpgradeScripts)
